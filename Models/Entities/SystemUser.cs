3a5b64b0176183e9ebfb6086610d609ab31a76da
[Massoud Ezat] <[massoudezat@gmail.com]>
2023-04-16 01:55:23 +0200

    Edit System User Phone

--- Models/Entities/SystemUser.cs
+++ Models/Entities/SystemUser.cs
@@ -6,7 +6,6 @@
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }   
-        public string Phone { get; set; }
         public string Role { get; set; }
         public bool IsActivated { get; set; }
         public string Name { get; set; }
