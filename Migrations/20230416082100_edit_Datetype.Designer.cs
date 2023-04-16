6a1aba15697b3332dc3a42e56b1ccc67c1e163f6
[Massoud Ezat] <[massoudezat@gmail.com]>
2023-04-16 10:29:32 +0200

    last Update On System User

--- Migrations/20230416082100_edit_Datetype.Designer.cs
+++ Migrations/20230416082100_edit_Datetype.Designer.cs
@@ -4,6 +4,7 @@ using Car4EgarAPI.Models.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Car4EgarAPI.Migrations
 {
     [DbContext(typeof(Car4EgarContext))]
-    partial class Car4EgarContextModelSnapshot : ModelSnapshot
+    [Migration("20230416082100_edit_Datetype")]
+    partial class edit_Datetype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
